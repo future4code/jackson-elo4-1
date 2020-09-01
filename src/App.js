2edd172937b1f2ff48162ca43167b9048c8266bb
Claudia Trevisan <ctrevisan.oliveira@gmail.com>
2020-09-01 16:00:49 -0300

    App implementado

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
 import React from "react";
 import axios from "axios";
 import styled from "styled-components";
+import LoginScreen from "./components/LoginScreen";
+import ProductGrid from "./components/ProductGrid";
+import SellerPanel from "./components/SellerPanel";
+import AddProduct from "./components/AddProduct"
+
+const ContainerApp = styled.main`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: auto;
+`;
+
 
 export default class App extends React.Component {
+  state={
+    pageRender: "pageDirection"
+  };
+
+  onClickClient = () =>{
+    this.setState({pageRender: "pageProducts"})
+  };
+
+  onClickSeller = () =>{
+    this.setState({pageRender: "pageSeller"})
+  };
+
+  onClickAddProducts = () =>{
+    this.setState({pageRender: "pageAddProducts"})
+  };
+
+
   render() {
+    const renderize = () =>{
+      if(this.state.pageRender === "pageDirection"){
+        return <LoginScreen functionOnClickCliente={this.onClickClient} functionOnClickSeller={this.onClickSeller}/>
+      }
+      else if(this.state.pageRender === "pageProducts"){
+        return <ProductGrid/>
+      }
+      else if(this.state.pageRender === "pageSeller"){
+        return <SellerPanel functionOnClickAdd={this.onClickAddProducts}/>
+      }
+      else if(this.state.pageRender === "pageAddProducts"){
+        return <AddProduct/>
+      }
+    };
+
     return (
       <div>
-        <p>Pronto para começar!</p>
+        {renderize()}
       </div>
     );
-  }
+  };
 }
