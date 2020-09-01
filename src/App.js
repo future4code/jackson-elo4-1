3e06e4fd040d46eb6f51ac4016030c1770426cdb
luccasbarros <ldbcandido@gmail.com>
2020-09-01 14:46:54 -0300

    Material Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from "react";
 import axios from "axios";
 import styled from "styled-components";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#f4f5f7",
+    },
+    secondary: {
+      main: "#fdb930",
+    },
+  },
+});
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
+        {/* <ThemeProvider theme={theme}>
+          <Button variant="contained" color="primary">
+          </Button>
+          <TextField
+            label="Nome do produto"
+            variant="standard"
+            color="secondary"
+          />
+        </ThemeProvider> */}
         <p>Pronto para começar!</p>
       </div>
     );
